ef124b4f1929ce4946205872e4e4fab3330c7c7d
Ali Akbar <sayaliakbar@gmail.com>
2025-03-16 01:03:04 +0500

    Add database connection on server start

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
@@ -16,10 +17,12 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(
     `Subscription Tracker API is running on http://localhost:${PORT}`
   );
+
+  await connectToDatabase();
 });
 
 export default express;
