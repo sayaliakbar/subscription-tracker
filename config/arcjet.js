d38b915c8df57f777ec3def230c82125a877fb86
Ali Akbar <sayaliakbar@gmail.com>
2025-03-16 12:32:20 +0500

    Update arcjet configuration to allow requests from Postman in bot detection

--- config/arcjet.js
+++ config/arcjet.js
@@ -9,7 +9,7 @@ const aj = arcjet({
 
     detectBot({
       mode: "LIVE",
-      allow: ["CATEGORY:SEARCH_ENGINE"],
+      allow: ["CATEGORY:SEARCH_ENGINE", "POSTMAN"],
     }),
 
     tokenBucket({
