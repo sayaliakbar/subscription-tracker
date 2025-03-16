0c99723c014730f2ce86a0c5ff199b19c7283c2b
Ali Akbar <sayaliakbar@gmail.com>
2025-03-16 06:12:44 +0500

    Fix API versioning in auth route path

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ import cookieParser from "cookie-parser";
 import { PORT } from "./config/env.js";
 
 import userRouter from "./routes/user.routes.js";
-import authRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 
@@ -15,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/api/vi/auth", authRouter);
+app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
