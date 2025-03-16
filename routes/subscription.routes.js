778e8cda22c48d5d55fb0f7ba8903f98b336b77f
Ali Akbar <sayaliakbar@gmail.com>
2025-03-16 12:32:39 +0500

    Add authorization middleware and update subscription routes for user access

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
 
+import authorize from "../middlewares/auth.middleware.js";
+
+import {
+  createSubscription,
+  getUserSubscriptions,
+} from "../controllers/subscription.controller.js";
+
 const subscriptionRouter = Router();
 
 subscriptionRouter.get("/", (req, res) => {
@@ -10,24 +17,20 @@ subscriptionRouter.get("/:id", (req, res) => {
   res.send({ title: "GET subscription details" });
 });
 
-subscriptionRouter.post("/", (req, res) => {
-  res.send({ title: "CREATE subscription" });
-});
+subscriptionRouter.post("/", authorize, createSubscription);
 
 subscriptionRouter.put("/:id", (req, res) => {
   res.send({ title: "UPDATE subscription" });
 });
 
 subscriptionRouter.delete("/:id", (req, res) => {
-  res.send({ title: "DELETE subscrption" });
+  res.send({ title: "DELETE subscription" });
 });
 
-subscriptionRouter.get("/user/:id", (req, res) => {
-  res.send({ title: "GET all user subscrptions" });
-});
+subscriptionRouter.get("/user/:id", authorize, getUserSubscriptions);
 
 subscriptionRouter.put("/:id/cancel", (req, res) => {
-  res.send({ title: "CANCEL subscrption" });
+  res.send({ title: "CANCEL subscription" });
 });
 
 subscriptionRouter.get("/upcoming-renewals", (req, res) => {
