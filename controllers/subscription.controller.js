258ce8bbf718475be7341aa139c1a36f5736329b
Ali Akbar <sayaliakbar@gmail.com>
2025-03-16 22:06:26 +0500

    Trigger workflow reminder on subscription creation

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,10 +1,23 @@
 import Subscription from "../models/subscription.model.js";
+import { workflowClient } from "../config/upstash.js";
+import { SERVER_URL } from "../config/env.js";
 
 export const createSubscription = async (req, res, next) => {
   try {
     const subscription = await Subscription.create({
       ...req.body,
       user: req.user._id,
+    });
+
+    await workflowClient.trigger({
+      url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+      body: {
+        subscriptionId: subscription.id,
+      },
+      headers: {
+        "content-type": "application/json",
+      },
+      retries: 0,
     });
 
     res.status(201).json({ success: true, data: subscription });
